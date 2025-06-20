13b1d6c51f8c585e115abb8b2430a5ba244442e0
Andy <andy.l.hunt85@gmail.com>
2025-06-20 06:45:30 -0700

    feat: update URL structure

--- models/recipe.js
+++ models/recipe.js
@@ -31,41 +31,43 @@ const recipeSchema = new mongoose.Schema({
 const Recipe = mongoose.model('Recipe', recipeSchema)
 
 // Below is an example of how to write a recipe
-/*
+
 const recipe = new Recipe(
     {
-        "name": "Buttermilk Biscuits",
-        "summary": "These biscuits bake up light and fluffy. It's important not to overwork the dough, which would make the biscuits tough. To that end, we pulse the butter and dry ingredients together in a food processor, then turn them out into a bowl and gradually work in the liquids by hand. You can serve them with some good butter and raspberry jam, and perhaps a sprinkle of fleur de sel, but they're so good you might want to eat them as is, straight out of the oven. We serve these with fried chicken, but they make a good brunch accompaniment and also work as a strawberry shortcake biscuit for dessert.",
+        "name": "Grissini",
+        "summary": "Simple breadsticks, fun to hold, satisfyingly crunchy. You can take these breadsticks in numerous directions, flattening them for a different texture, or using different oils - garlic rather than plain olive oil, for instance - different seasoning, or chopped herbs. These go well with salads, such as a tomato salad where you want some contrasting texture.",
         "ingredients": [
-            "2 cups cake flour",
-            "2 cups all-purpose flour",
-            "1 tablespoon kosher salt",
-            "1 tablespoon baking powder",
-            "1 teaspoon baking soda",
-            "0.5 pound (2 sticks) unsalted butter, cut into half-inch cubes and chilled",
-            "1.5 cups buttermilk, plus 1 to 2 tablespoons for brushing",
-            "2 to 3 tablespoons (1 to 1.5 ounces) unsalted butter, melted"
+            "1/2 cup warm water (110 to 115 degrees)",
+            "1 tablespoon active dry yeast (not quick-rising)",
+            "180 grams (1.5 cups) all-purpose flour, plus additional as needed",
+            "54 grams (1/3 cup) fine semolina flour",
+            "1/4 cup freshly grated Parmigiano-Reggiano",
+            "1 teaspoon ground fleur de sel or fine sea salt",
+            "2 tablespoons olive oil, plus additional for brushing",
+            "Coarsely ground black pepper"
         ],
         "originalInstructions": [
-            "Preheat the oven to 425 degrees. Line a baking sheet with parchment paper.",
-            "Combine the flours, salt, baking powder, and baking soda in the bowl of a food processor and pulse a few times to blend. Add the chilled butter and pulse several times, until the pieces of butter are no bigger than small peas. Do not overprocess; the dough should not come together.",
-            "Transfer the dough to a large bowl and make a well in the center of the flour mixture. Pour in the buttermilk. Stir and lift the mixture with a sturdy spoon, gently working the flour into the buttermilk. The dough should begin to come together but not form a solid mass, or the biscuits may be tough.",
-            "Dust a work surface with flour and turn out the dough. Pat the dough into a 3/4-inch-thick rectangle. Using a 2.5-inch round cutter, cut out the biscuits. (If the cutter sticks to the dough, dip the cutter in flour before cutting.) Place the biscuits on the baking sheet. The dough trimmings can be gently pushed together, patted out, and cut one more time; do not overwork the dough.",
-            "Brush the tops of the dough lightly with buttermilk. Bake for 15 to 18 minutes, rotating the pan halfway through baking, until a rich golden brown. As soon as you remove the biscuits from the oven, brush the tops with melted butter. Serve warm."
+            "Combine the water and yeast in a small bowl. Let stand 10 minutes, than stir until the yeast is completely dissolved.",
+            "Combine the flours, cheese, and salt in a large bowl. Make a well in the center of the dry ingredients. Stir the oil into the yeast mixture, then pour into the well and mix together with a fork. Once the dough comes together, transfer to a lightly floured board and knead, adding a dusting of all-purpose flour as necessary (depending on the flours and humidity, you may need up to an additional 1/4 cup), until a smooth dough forms. Shape the dough into a ball and roll on the board to coat very lightly with flour.",
+            "Transfer the dough to a medium bowl, cover with a damp towel, and let rest in a warm place for about 15 minutes, or until it has risen slightly.",
+            "Position the oven racks in the lower and upper thirds of the oven and preheat the oven to 400 degrees. Line two baking sheets with parchment paper.",
+            "Turn the dough out onto the floured work surface and, using a dough scraper or sharp knife, cut the dough in half. Cut one half into 12 pieces for short grissini, or 6 pieces for long grissini. Roll each piece into a rope about 9 inches long for short grissini, or about 15 inches long for long grissini. You can leave them round or twist or flatten them, or a combination of the two. The thinner or flatter they are, the crispier the result; thicker grissini may be doughier, if that is your preference. Transfer to one of the parchment-lined baking sheets as you form them.",
+            "Coarsely grind a light dusting of pepper onto a section of the work surface; it is easier to control the amount of pepper that will be rolled onto the grissini by keeping the area small. (Alternatively, omit the pepper and sprinkle the grissini with fleur de sel or seed.) Light brush the grissini with olive oil. One at a time, roll in the pepper and return to the parchment; grind additional pepper as needed. Repeat with the remaining dough. (The grissini can be held for up to an hour on the baking sheets in a cool spot. Or, to hold them longer, cover with parchment paper, wrap the baking sheets in plastic wrap, and refrigerate for up to 6 hours.",
+            "Bake the grissini, switching the position of the pans and rotating them halfway through baking, until golden and crisp, 16 to 18 minutes. Cool on the pans in a rack. Store in an airtight container for up to 1 week."
             ],
         "updatedInstructions": [
-            "Preheat the oven to 425 degrees. Line a baking sheet with parchment paper. Place the butter cut into cubes in the freezer.",
-            "Combine the flours, salt, baking powder, and baking soda in the bowl of a food processor and pulse a few times to blend. Add the chilled butter and pulse 5 times, holding one second per pulse, until the pieces of butter are no bigger than small peas. The dough should not come together.",
-            "Transfer the dough to a large bowl and make a well in the center of the flour mixture. Pour in the buttermilk. Makre sure to empty all the buttermilk into the well. Stir and lift the mixture with a sturdy spoon, gently working the flour into the buttermilk. The dough should begin to come together but not form a solid mass, or the biscuits may be tough.",
-            "Dust a work surface with flour and turn out the dough. Pat the dough into a 3/4-inch-thick rectangle; do not knead. Divide into four and stack each square on top of each other; use a rolling pin to gently roll out into a rectangle again about 2 inches tall",
-            "Set aside a small bowl of flour. Using a 2.5-inch round cutter, cut out the biscuits; if the cutter sticks to the dough, dip the cutter in flour before cutting. Place the biscuits on the baking sheet. The dough trimmings can be gently pushed together, patted out, and cut one more time",
-            "Brush the tops of the dough lightly with buttermilk. Bake for 15 to 18 minutes, rotating the pan halfway through baking, until a rich golden brown. As soon as you remove the biscuits from the oven, brush half the tops with melted butter; Alexi prefers several to not have the additional butter. Add fleur de sel salt, if desired. Serve warm."
+            "Combine the water and yeast in a small bowl. Let stand 10 minutes, than stir until the yeast is completely dissolved.",
+            "Combine the flours, cheese, and salt in a large bowl. Make a well in the center of the dry ingredients. Stir the oil into the yeast mixture, then pour into the well and mix together with a fork. Once the dough comes together, transfer to a lightly floured board and knead, adding a dusting of all-purpose flour as necessary (depending on the flours and humidity, you may need up to an additional 1/4 cup), until a smooth dough forms. Shape the dough into a ball and roll on the board to coat very lightly with flour.",
+            "Transfer the dough to a medium bowl, cover with a damp towel, and let rest in a warm place for about 15 minutes, or until it has risen slightly.",
+            "Position the oven racks in the lower and upper thirds of the oven and preheat the oven to 400 degrees. Line two baking sheets with parchment paper.",
+            "Turn the dough out onto the floured work surface and, using a dough scraper or sharp knife, cut the dough in half. Cut one half into 12 pieces for short grissini, or 6 pieces for long grissini. Roll each piece into a rope about 9 inches long for short grissini, or about 15 inches long for long grissini. You can leave them round or twist or flatten them, or a combination of the two. The thinner or flatter they are, the crispier the result; thicker grissini may be doughier, if that is your preference. Transfer to one of the parchment-lined baking sheets as you form them.",
+            "Coarsely grind a light dusting of pepper onto a section of the work surface; it is easier to control the amount of pepper that will be rolled onto the grissini by keeping the area small. (Alternatively, omit the pepper and sprinkle the grissini with fleur de sel or seed.) Light brush the grissini with olive oil. One at a time, roll in the pepper and return to the parchment; grind additional pepper as needed. Repeat with the remaining dough. (The grissini can be held for up to an hour on the baking sheets in a cool spot. Or, to hold them longer, cover with parchment paper, wrap the baking sheets in plastic wrap, and refrigerate for up to 6 hours.",
+            "Bake the grissini, switching the position of the pans and rotating them halfway through baking, until golden and crisp, 16 to 18 minutes. Cool on the pans in a rack. Store in an airtight container for up to 1 week."
             ],
-            "amount": "Makes 12 biscuits",
-        "recipeId": 2,
+            "amount": "Makes 24 small or 12 large grissini",
+        "recipeId": 4,
     },
 )
-*/
 
 if (process.argv.length < 4) {
     Recipe.find({}).then(result => {
